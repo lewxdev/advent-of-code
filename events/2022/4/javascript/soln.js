84a021ac7a2b42bff2dc9c79812d50f6b10e9594
J. Lewis <inquiries.lewis+dev@gmail.com>
2022-12-04 00:35:45 -0500

    2022-04 ⭐⭐

--- events/2022/4/javascript/soln.js
+++ events/2022/4/javascript/soln.js
@@ -1,16 +1,16 @@
 // Find the challenge here: https://adventofcode.com/2022/day/4
 
+/** @param {string} sections */
+const expand = (sections) => {
+  const [start, end] = sections.split("-").map((section) => parseInt(section))
+  const result = []
+
+  for (let n = start; n <= end; n++) result.push(n)
+  return result
+}
+
 /** @param {string} input - the provided puzzle input string */
 exports.campCleanupPart1 = (input) => {
-  /** @param {string} sections */
-  const expand = (sections) => {
-    const [start, end] = sections.split("-").map((section) => parseInt(section))
-    const result = []
-
-    for (let n = start; n <= end; n++) result.push(n)
-    return result
-  }
-
   return input.trim().split("\n").reduce((result, pairing) => {
     const [assignmentA, assignmentB] =
       pairing
@@ -25,7 +25,19 @@ exports.campCleanupPart1 = (input) => {
 }
 
 /** @param {string} input - the provided puzzle input string */
-exports.campCleanupPart2 = (input) => null
+exports.campCleanupPart2 = (input) => {
+  return input.trim().split("\n").reduce((result, pairing) => {
+    const [assignmentA, assignmentB] =
+      pairing
+        .split(",")
+        .map(expand)
+        .sort((a, b) => b.length - a.length)
+
+    return assignmentA.some((section) => assignmentB.includes(section))
+      ? ++result
+      : result
+  }, 0)
+}
 
 
 if (require.main === module) {
--- events/2022/4/javascript/soln.test.js
+++ events/2022/4/javascript/soln.test.js
@@ -2,10 +2,10 @@ const { getUniquePuzzleInput, validateSolutions } = require("testers/javascript"
 const fns = require("./soln")
 
 const EXAMPLE_INPUT = "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8\n"
-const EXAMPLE_OUTPUT = [2, null]
+const EXAMPLE_OUTPUT = [2, 4]
 
 const PUZZLE_INPUT = getUniquePuzzleInput("2022/4")
-const PUZZLE_OUTPUT = [584, null]
+const PUZZLE_OUTPUT = [584, 933]
 
 describe("[2022/4]", () => {
   describe("examples", validateSolutions(fns, EXAMPLE_INPUT, ...EXAMPLE_OUTPUT))
