6962d05f5a8b2cd358a2a11fc23ff884ddb9d9e8
J. Lewis <inquiries.lewis+dev@gmail.com>
2022-12-05 21:35:50 -0500

    2022-05 (Refined solution)

--- events/2022/5/javascript/soln.js
+++ events/2022/5/javascript/soln.js
@@ -1,42 +1,40 @@
 // Find the challenge here: https://adventofcode.com/2022/day/5
 
 /**
- * @param {9000|9001} model
- * @returns {(input: string) => string}
+ * @param {9000|9001} model - a supported crane model number
+ * @returns {(input: string) => string} the respective solution function
  */
 const craneProcedureExecution = (model) => (input) => {
-  const [initialVisual, procedure] = input.split("\n\n")
+  const [representation, procedure] = input.split("\n\n")
 
   /** @type {string[][]} */
   const stacks =
-    initialVisual
+    representation
       .split("\n")
       .reverse()
-      .reduce(
-        /** @param {string[][]} result */
-        (result, line, i) => {
-        if (i === 0) return [...Array(parseInt(line.at(-2)))].map(() => [])
+      .reduce((result, line, lineIndex) => {
+        if (lineIndex === 0)
+          return Array.from(Array(Number(line.at(-2))), () => [])
 
-        line.split("").forEach((char, j) => {
-          if (char.match(/[A-Z]/)) result[(j - 1) / 4].push(char)
-        })
+        for (let crateIndex = 1; crateIndex < line.length; crateIndex += 4)
+          if (line[crateIndex].match(/[A-Z]/))
+            result[(crateIndex - 1) / 4].push(line[crateIndex])
 
         return result
       }, [])
 
   procedure.trim().split("\n").forEach((step) => {
-    const [amount, from, to] = step.match(/\d+/g).map((match) => parseInt(match))
-    const crates = stacks[from - 1].splice(-amount, amount)
-    stacks[to - 1].push(...(model === 9001 ? crates : crates.reverse()))
+    const [amount, from, to] = step.match(/\d+/g).map(Number)
+    const selection = stacks[from - 1].splice(-amount, amount)
+
+    stacks[to - 1].push(...(model === 9001 ? selection : selection.reverse()))
   })
 
   return stacks.reduce((result, stack) => result += stack.at(-1) ?? "", "")
 }
 
-/** @param {string} input - the provided puzzle input string */
 exports.supplyStacksPart1 = craneProcedureExecution(9000)
 
-/** @param {string} input - the provided puzzle input string */
 exports.supplyStacksPart2 = craneProcedureExecution(9001)
 
 
