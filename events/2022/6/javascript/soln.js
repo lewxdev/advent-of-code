7c5f45a83ae1400693785a7e68cd1be4d516bfff
J. Lewis <inquiries.lewis+dev@gmail.com>
2022-12-06 01:04:08 -0500

    2022-06 ⭐⭐

--- events/2022/6/javascript/soln.js
+++ events/2022/6/javascript/soln.js
@@ -1,14 +1,20 @@
 // Find the challenge here: https://adventofcode.com/2022/day/6
 
-/** @param {string} input - the provided puzzle input string */
-exports.tuningTroublePart1 = (input) => {
-  for (let endIndex = 4; endIndex < input.length; endIndex++)
-    if (new Set(input.slice(endIndex - 4, endIndex)).size === 4)
-      return endIndex
+/**
+ * @param {number} delimiterLength - the length of the delimiter to search for
+ * @returns {(input: string) => number} the respective solution function
+ */
+const getFirstDelimiterIndex = (delimiterLength) => (input) => {
+  for (let endIndex = delimiterLength; endIndex < input.length; endIndex++) {
+    const substring = input.slice(endIndex - delimiterLength, endIndex)
+
+    if (new Set(substring).size === delimiterLength) return endIndex
+  }
 }
 
-/** @param {string} input - the provided puzzle input string */
-exports.tuningTroublePart2 = (input) => {}
+exports.tuningTroublePart1 = getFirstDelimiterIndex(4)
+
+exports.tuningTroublePart2 = getFirstDelimiterIndex(14)
 
 
 if (require.main === module) {
