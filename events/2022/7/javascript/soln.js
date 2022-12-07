723c9190cc8f2a7188de9ab9982438bef7128b53
J. Lewis <inquiries.lewis+dev@gmail.com>
2022-12-07 14:21:54 -0500

    2022-07 ⭐⭐

--- events/2022/7/javascript/soln.js
+++ events/2022/7/javascript/soln.js
@@ -1,48 +1,63 @@
 // Find the challenge here: https://adventofcode.com/2022/day/7
 
-const { join } = require("path")
-
-const COMMAND_PATTERN = /^\$\s(?:cd\s(?<directory>.+)|ls)$/
-const OUTPUT_PATTERN = /^(?:dir\s(?<directory>.+)|(?<filesize>\d+)\s(?<filename>.+))$/
-
 /** @typedef {{ size: number; ".."?: string; }} Directory */
 
-/** @type {{ [path: string]: Directory }} */
-const files = { "/": { size: 0 } }
+/** @param {string} input */
+const getFilePaths = (input) => {
+  const { join } = require("path")
 
-/**
- * @param {string} path
- * @param {(path: Directory) => void} callback
- */
-const traverse = (path, callback) => {
-  if (!path) return
+  const COMMAND_PATTERN = /^\$\s(?:cd\s(?<directory>.+)|ls)$/
+  const OUTPUT_PATTERN = /^(?:dir\s(?<directory>.+)|(?<fileSize>\d+)\s.+)$/
 
-  callback(files[path])
-  traverse(files[path][".."], callback)
-}
+  /** @type {{ [path: string]: Directory }} */
+  const filePaths = { "/": { size: 0 } }
 
-/** @param {string} input - the provided puzzle input string */
-exports.noSpaceLeftOnDevicePart1 = (input) => {
+  /**
+   * @param {string=} path
+   * @param {(path: Directory) => void} callback
+   */
+  const traverse = (path, callback) => {
+    if (!path) return
+
+    callback(filePaths[path])
+    traverse(filePaths[path][".."], callback)
+  }
+
   input.trim().split("\n").reduce(({ parent }, line) => {
     if (line.startsWith("$")) {
       const { directory } = line.match(COMMAND_PATTERN).groups
       return { parent: !directory ? parent : join(parent, directory) }
     }
 
-    const { directory, filesize } = line.match(OUTPUT_PATTERN).groups
+    const { directory, fileSize } = line.match(OUTPUT_PATTERN).groups
 
-    if (directory) files[join(parent, directory)] = { size: 0, "..": parent }
-    else traverse(parent, (path) => path.size += parseInt(filesize))
+    if (directory)
+      filePaths[join(parent, directory)] = { size: 0, "..": parent }
+    else traverse(parent, (path) => path.size += parseInt(fileSize))
+
     return { parent }
   }, { parent: "/" })
 
-  return Object.values(files).reduce((result, { size }) =>
-    size <= 100_000 ? result += size : result, 0
-  )
+  return filePaths
 }
 
 /** @param {string} input - the provided puzzle input string */
-exports.noSpaceLeftOnDevicePart2 = (input) => {}
+exports.noSpaceLeftOnDevicePart1 = (input) =>
+  Object.values(getFilePaths(input)).reduce((result, { size }) =>
+    size <= 100_000 ? result += size : result, 0
+  )
+
+/** @param {string} input - the provided puzzle input string */
+exports.noSpaceLeftOnDevicePart2 = (input) => {
+  const filePaths = getFilePaths(input)
+  const minFileSize = 30_000_000 - (70_000_000 - filePaths["/"].size)
+
+  return Object
+    .values(filePaths)
+    .sort((a, b) => a.size - b.size)
+    .find(({ size }) => size >= minFileSize)
+    .size
+}
 
 if (require.main === module) {
   const { testSolutions } = require("testers/javascript")
