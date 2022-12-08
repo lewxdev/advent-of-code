cae229c666b9bb5cc154281841d09d8096e782f4
J. Lewis <inquiries.lewis+dev@gmail.com>
2022-12-08 03:58:21 -0500

    2022-08 ⭐⭐

--- events/2022/8/javascript/soln.js
+++ events/2022/8/javascript/soln.js
@@ -1,69 +1,99 @@
 // Find the challenge here: https://adventofcode.com/2022/day/8
 
 /** @param {string} input - the provided puzzle input string */
-exports.treetopTreeHousePart1 = (input) => {
-  const matrix =
-    input
-      .trim()
-      .split("\n")
-      .reduce(
-        /** @param {number[][]} result */
-        (result, row) => [...result, row.split("").map((v) => parseInt(v))], []
-      )
+const buildHeightMap = (input) =>
+  input
+    .trim()
+    .split("\n")
+    .reduce(
+      /** @param {number[][]} result */
+      (result, row) => [...result, Array.from(row.split(""), Number)], []
+    )
 
-  const numRows = matrix.length
-  const numCols = matrix[0].length
+/**
+ * @param {number[][]} heightMap
+ * @param {"left" | "right" | "up" | "down"} direction
+ * @param {number} initialY
+ * @param {number} initialX
+ */
+const getHeightMapSlice = (heightMap, direction, initialY, initialX) => {
+  switch (direction) {
+    case "left":
+      return heightMap[initialY].slice(0, initialX).reverse()
+    case "right":
+      return heightMap[initialY].slice(initialX + 1)
+    case "up":
+      return heightMap
+        .slice(0, initialY)
+        .reverse()
+        .reduce((result, row) => [...result, row[initialX]], [])
+    case "down":
+      return heightMap
+        .slice(initialY + 1)
+        .reduce((result, row) => [...result, row[initialX]], [])
+    default:
+      throw `Invalid direction argument: "${direction}"`
+  }
+}
+
+/**
+ * @param {number[][]} heightMap
+ * @param {number} y
+ * @param {number} x
+ */
+const getTreeVisibility = (heightMap, y, x) =>
+  !!["up", "left", "right", "down"].find((direction) =>
+    getHeightMapSlice(heightMap, direction, y, x)
+      .every((height) => height < heightMap[y][x])
+  )
+
+/**
+ * @param {number[][]} heightMap
+ * @param {number} y
+ * @param {number} x
+ */
+const getScenicScore = (heightMap, y, x) =>
+  ["up", "left", "right", "down"]
+    .map((direction) => {
+      let viewDistance = 0
+      for (const height of getHeightMapSlice(heightMap, direction, y, x))
+        if (height < heightMap[y][x]) viewDistance++
+        else return ++viewDistance
+
+      return viewDistance
+    })
+    .reduce((acc, viewDistance) => acc * viewDistance)
+
+/** @param {string} input - the provided puzzle input string */
+exports.treetopTreeHousePart1 = (input) => {
+  const heightMap = buildHeightMap(input)
+  const boundY = heightMap.length
+  const boundX = heightMap[0].length
 
   // Assume all trees on perimeter are visible
-  let visibilityCount = ((numRows - 2) * 2) + (numCols * 2)
+  let visibilityCount = ((boundY - 2) * 2) + (boundX * 2)
 
-  for (let rowIndex = 1; rowIndex < numRows - 1; rowIndex++)
-    innerLoop:
-    for (let colIndex = 1; colIndex < numCols - 1; colIndex++) {
-      const tree = matrix[rowIndex][colIndex]
-      /** @param {number[]} array */
-      const getIsVisible = (array) => array.every((height) => height < tree)
-
-      const leftmostTrees = matrix[rowIndex].slice(0, colIndex)
-
-      if (getIsVisible(leftmostTrees)) {
-        visibilityCount++
-        continue innerLoop
-      }
-
-      const rightmostTrees = matrix[rowIndex].slice(colIndex + 1)
-
-      if (getIsVisible(rightmostTrees)) {
-        visibilityCount++
-        continue innerLoop
-      }
-
-      const uppermostTrees =
-        matrix
-          .slice(0, rowIndex)
-          .reduce((result, row) => [...result, row[colIndex]], [])
-
-      if (getIsVisible(uppermostTrees)) {
-        visibilityCount++
-        continue innerLoop
-      }
-
-      const lowermostTrees =
-        matrix
-          .slice(rowIndex + 1)
-          .reduce((result, row) => [...result, row[colIndex]], [])
-
-      if (getIsVisible(lowermostTrees)) {
-        visibilityCount++
-        continue innerLoop
-      }
-    }
+  for (let y = 1; y < boundY - 1; y++)
+    for (let x = 1; x < boundX - 1; x++)
+      if (getTreeVisibility(heightMap, y, x)) visibilityCount++
 
   return visibilityCount
 }
 
 /** @param {string} input - the provided puzzle input string */
-exports.treetopTreeHousePart2 = (input) => null
+exports.treetopTreeHousePart2 = (input) => {
+  const heightMap = buildHeightMap(input)
+  const boundY = heightMap.length
+  const boundX = heightMap[0].length
+
+  const scores = []
+
+  for (let y = 1; y < boundY - 1; y++)
+    for (let x = 1; x < boundX - 1; x++)
+      scores.push(getScenicScore(heightMap, y, x))
+
+  return scores.sort((a, b) => b - a).at(0)
+}
 
 
 if (require.main === module) {
--- events/2022/8/javascript/soln.test.js
+++ events/2022/8/javascript/soln.test.js
@@ -2,10 +2,10 @@ const { getUniquePuzzleInput, validateSolutions } = require("testers/javascript"
 const solutions = require("./soln")
 
 const EXAMPLE_INPUT = "30373\n25512\n65332\n33549\n35390\n"
-const EXAMPLE_OUTPUT = [21, null]
+const EXAMPLE_OUTPUT = [21, 8]
 
 const PUZZLE_INPUT = getUniquePuzzleInput("2022/8")
-const PUZZLE_OUTPUT = [1_713, null]
+const PUZZLE_OUTPUT = [1_713, 268_464]
 
 describe("[2022/8]", () => {
   describe("examples", validateSolutions(solutions, EXAMPLE_INPUT, EXAMPLE_OUTPUT))
