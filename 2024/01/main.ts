bf7a3b27b22bf58f985f9b12289e9d6dcb402795
J. Lewis <6710419+lewxdev@users.noreply.github.com>
2024-12-07 23:44:58 -0500

    feat: 2024 day 1 part 1

---
+++ 2024/01/main.ts
@@ -0,0 +1,31 @@
+// see: https://adventofcode.com/2024/day/1
+
+console.log("--- Day 1: Historian Hysteria ---");
+const input = await Deno.readTextFile("./2024/01/input.txt");
+
+const parseInput = () =>
+  input
+    .trim()
+    .split("\n")
+    .reduce<[listA: number[], listB: number[]]>(([listA, listB], line) => {
+      const [left, right] = line.split(/\s+/).map(Number);
+      return [listA.concat(left), listB.concat(right)];
+    }, [[], []]);
+
+function part1(): string {
+  const [listA, listB] = parseInput();
+  listA.sort();
+  return listB
+    .sort()
+    .reduce((sum, value, index) => sum + Math.abs(value - listA[index]), 0)
+    .toString();
+}
+
+function part2(): string {
+  // add your answer here
+  return "";
+}
+
+export default function (part: "1" | "2"): string {
+  return part === "1" ? part1() : part2();
+}
