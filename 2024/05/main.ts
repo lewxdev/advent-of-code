fbb45a008bada22108fce7d2f526f2e7052fcfd4
J. Lewis <6710419+lewxdev@users.noreply.github.com>
2024-12-09 11:21:50 -0500

    feat: 2024 day 5 part 2

--- 2024/05/main.ts
+++ 2024/05/main.ts
@@ -4,43 +4,52 @@ console.log("--- Day 5: Print Queue ---");
 const input = await Deno.readTextFile("./2024/05/input.txt");
 
 const parseInput = () => {
-  const sections = input
+  const [rulesSection, updatesSection] = input
     .trim()
     .split("\n\n")
     .map((section) => section.split("\n"));
-  return sections.map((section, index) =>
-    index
-      ? section.map((update) => update.split(",").map(Number))
-      : section.map((rule) => {
-        const [before, after] = rule.split("|").map(Number);
-        return { before, after };
-      })
-  ) as [rules: { before: number; after: number }[], updates: number[][]];
+  return {
+    rules: rulesSection.map((rule) =>
+      rule.split("|").map(Number) as [before: number, after: number]
+    ),
+    updates: updatesSection.map((update) => update.split(",").map(Number)),
+  };
 };
 
+const filterUpdates = (
+  { rules, updates }: ReturnType<typeof parseInput>,
+  options: { isValid: boolean },
+) =>
+  updates.filter((update) =>
+    options.isValid ===
+      update.every((page, index) =>
+        rules.every(([before, after]) =>
+          page !== before ||
+          !update.includes(after) ||
+          update.slice(index + 1).includes(after)
+        )
+      )
+  );
+
 function part1(): string {
-  const [rules, updates] = parseInput();
-  const groupedRules = Object.groupBy(rules, ({ before }) => before);
-
-  return updates
-    .reduce((sum, update) =>
-      update.every((page, index) => {
-          const pageRules = groupedRules[page];
-          if (!pageRules) return true;
-
-          const slice = update.slice(index + 1);
-          return pageRules.every(({ after }) =>
-            !update.includes(after) || slice.includes(after)
-          );
-        })
-        ? sum + update[Math.floor(update.length / 2)]
-        : sum, 0)
+  return filterUpdates(parseInput(), { isValid: true })
+    .reduce((sum, update) => sum + update[Math.floor(update.length / 2)], 0)
     .toString();
 }
 
 function part2(): string {
-  // add your answer here
-  return "";
+  const { rules, updates } = parseInput();
+  return filterUpdates({ rules, updates }, { isValid: false })
+    .reduce((sum, update) => {
+      update.sort((a, b) => {
+        const [[before]] = rules.filter((rule) =>
+          rule.includes(a) && rule.includes(b)
+        );
+        return before === a ? -1 : 1;
+      });
+      return sum + update[Math.floor(update.length / 2)];
+    }, 0)
+    .toString();
 }
 
 export default function (part: "1" | "2"): string {
